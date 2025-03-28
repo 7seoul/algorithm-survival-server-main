a332598b7ce8ad1347ca9dcf8e9f313a3be0b342
gonudayo <kgw010119@gmail.com>
2025-03-28 16:43:05 +0900

    fix: jest setup

--- server/app.js
+++ server/app.js
@@ -1,19 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-
-dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-// MongoDB 연결
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((e) => console.log("MongoDB error: ", e));
 
 // API 라우트 설정 v1
 const usersV1 = require("./src/v1/routes/users");
--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 const app = require("./app");
 const cron = require("node-cron");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+// MongoDB 연결
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((e) => console.log("MongoDB error: ", e));
 
 // 유저 정보 자동 업데이트
 const update = require("./src/services/update");
