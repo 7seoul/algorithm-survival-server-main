492cc926fc3d65206de35e7ad3d7f86fd1140d57
gonudayo <kgw010119@gmail.com>
2025-04-05 17:33:20 +0900

    feat: winston init

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const logger = require("./logger");
 
 dotenv.config();
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  logger.info(`Server listening on port ${port}`);
 });
 
 // MongoDB 연결
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((e) => console.log("MongoDB error: ", e));
+  .then(() => logger.info("Connected to MongoDB"))
+  .catch((e) => logger.info("MongoDB error: ", e));
 
 // 유저 정보 자동 업데이트
 const autoUpdate = require("./src/services/autoUpdate");
