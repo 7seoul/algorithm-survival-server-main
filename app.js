e3e7f67f28f0f76773abc5e7bd0668e897eeb727
gonudayo <kgw010119@gmail.com>
2025-04-08 09:27:25 +0900

    fix: cors

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const cookieParser = require("cookie-parser");
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+
+const corsOptions = {
+  origin: "https://survialgo.netlify.app",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 // API 라우트 설정 v2
 const users = require("./src/v2/routes/users");
@@ -15,7 +21,7 @@ const rankings = require("./src/v2/routes/rankings");
 const search = require("./src/v2/routes/search");
 
 app.use("/api/v2/users", users);
-app.use("/api/v2/auth", auth)
+app.use("/api/v2/auth", auth);
 app.use("/api/v2/groups", groups);
 app.use("/api/v2/rankings", rankings);
 app.use("/api/v2/search", search);
