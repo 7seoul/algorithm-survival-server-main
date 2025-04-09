f3eeb2ac464199c01eb2f835418818742a2e002c
gonudayo <kgw010119@gmail.com>
2025-04-09 15:05:28 +0900

    fix: search 분리

--- src/v2/routes/search/index.js
+++ src/v2/routes/search/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const ctrl = require("./search.ctrl");
 
-router.get("/", ctrl.get.all); // 유저, 그룹 검색
+router.get("/groups", ctrl.get.groups); // 그룹 검색
+router.get("/users", ctrl.get.users); // 유저 검색
 
 module.exports = router;
--- src/v2/routes/search/search.ctrl.js
+++ src/v2/routes/search/search.ctrl.js
@@ -3,39 +3,20 @@ const { Group } = require("../../../models/Group/Group");
 const logger = require("../../../../logger");
 
 const get = {
-  all: async (req, res) => {
+  users: async (req, res) => {
     try {
-      const { all } = req.query; // 검색어 받기
+      const { q } = req.query; // 검색어 받기
 
-      if (!all) {
+      if (!q) {
         return res.status(400).json({ error: "검색어를 입력하세요." });
       }
-
-      const groups = await Group.aggregate([
-        {
-          $search: {
-            index: "groupSearchIndex",
-            autocomplete: {
-              query: all,
-              path: "groupName",
-            },
-          },
-        },
-
-        {
-          $project: {
-            groupName: 1,
-          },
-        },
-        { $limit: 10 },
-      ]);
 
       const users = await User.aggregate([
         {
           $search: {
             index: "userSearchIndex",
             autocomplete: {
-              query: all,
+              query: q,
               path: "name",
             },
           },
@@ -53,8 +34,45 @@ const get = {
 
       return res.status(200).json({
         success: true,
-        groups,
         users,
+      });
+    } catch (error) {
+      logger.error(error);
+      return res
+        .status(500)
+        .json({ success: false, message: "서버 오류 발생" });
+    }
+  },
+  groups: async (req, res) => {
+    try {
+      const { q } = req.query; // 검색어 받기
+
+      if (!q) {
+        return res.status(400).json({ error: "검색어를 입력하세요." });
+      }
+
+      const groups = await Group.aggregate([
+        {
+          $search: {
+            index: "groupSearchIndex",
+            autocomplete: {
+              query: q,
+              path: "groupName",
+            },
+          },
+        },
+
+        {
+          $project: {
+            groupName: 1,
+          },
+        },
+        { $limit: 10 },
+      ]);
+
+      return res.status(200).json({
+        success: true,
+        groups,
       });
     } catch (error) {
       logger.error(error);
