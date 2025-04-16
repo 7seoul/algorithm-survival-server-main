489189f994ba2c3de3f573b877ddda2d41c35617
gonudayo <kgw010119@gmail.com>
2025-04-16 16:53:57 +0900

    fix: 점수배점

--- src/utils/calculator.js
+++ src/utils/calculator.js
@@ -3,8 +3,8 @@ const conversionCriteria = [
   0,                           // NR
   1, 1, 2, 3, 3,               // Bronze B5~B1
   5, 7, 9, 12, 15,             // Silver S5~S1
-  18, 21, 24, 27, 30,          // Gold G5~G1
-  40, 50, 60, 70, 80,          // Platinum P5~P1
+  18, 21, 24, 28, 32,          // Gold G5~G1
+  36, 40, 44, 50, 60,          // Platinum P5~P1
   100, 150, 200, 250, 300,     // Diamond D5~D1
   1000, 1500, 2000, 2500, 3000 // Ruby R5~R1
 ];
