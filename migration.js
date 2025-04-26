74c6bd256d5a4b6970bca0d2fd77391131957c9a
gonudayo <kgw010119@gmail.com>
2025-04-26 22:33:27 +0900

    feat: restore group member

--- migration.js
+++ migration.js
@@ -5,6 +5,38 @@ const { UserVerification } = require("./src/models/User/UserVerification");
 const solved = require("./src/apis/solvedac");
 const logger = require("./logger");
 
+const restoreMember = async () => {
+  const users = await User.find({});
+  const members = await MemberData.find({});
+
+  for (const user of users) {
+    const isMatched = await members.some((member) =>
+      member.user.equals(user._id)
+    );
+
+    if (isMatched) {
+      continue;
+    }
+
+    const memberData = new MemberData({
+      user: user._id,
+      initialStreak: user.initialStreak,
+      initialCount: user.initialCount,
+      initial: user.initial,
+      score: user.score,
+      count: user.count,
+    });
+    await memberData.save();
+
+    // 그룹 업데이트
+    await Group.findByIdAndUpdate(1, {
+      $addToSet: {
+        memberData: memberData._id,
+      },
+    });
+  }
+};
+
 const migrateScore = async () => {
   const users = await User.find({});
 
@@ -117,6 +149,7 @@ const migrateUserVerifications = async () => {
 };
 
 module.exports = {
+  restoreMember,
   migrateScore,
   migrateGroups,
   migrateMemberDatas,
