f258946b37705b3aa3a213f607687b8cf0f4019d
gonudayo <kgw010119@gmail.com>
2025-04-29 23:55:39 +0900

    delete: status api

--- src/v2/routes/status/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-const ctrl = require("./status.ctrl");
-
-router.get("/scrap", ctrl.get.scrap); // 스크래핑 상태
-
-module.exports = router;
